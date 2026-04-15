6dcaa960451331d77a21f8744bb336eb8defee51
Xhale2 <xhale2@uic.edu>
2026-04-15 17:19:22 -0500

    Tables for client, address, and credit card
    *Not finished*

---
+++ relationalSchema.sql
@@ -0,0 +1,28 @@
+CREATE TABLE CreditCard {  --strong entity
+    CreditCardNumber VARCHAR(30),
+    ClientEmail VARCHAR(100),
+    BillingAddressID INT,
+    PRIMARY KEY (CreditCardNumber),
+    FOREIGN KEY (ClientEmail) REFERENCES Client(Email),
+    --FOREIGN KEY (BillingAddressID) REFERENCES Address(zipcode)?
+};
+
+CREATE TABLE Address {   --strong entity
+    Street VARCHAR(50),
+    City VARCHAR(50),
+    Number INT,
+    CreditCardNumber INT,
+    ClientEmail VARCHAR(100),
+    Hotel VARCHAR(100),
+    --Zipcode INT,
+    --PRIMARY KEY (Zipcode),
+    FOREIGN KEY (ClientEmail) REFERENCES Client(Email),
+    FOREIGN KEY (Hotel) REFERENCES Hotel(HoetelID),
+    FOREIGN KEY (CreditCardNumber) REFERENCES CreditCard(CreditCardNumber)
+};
+
+CREATE TABLE Client {   --strong entity
+    Name VARCHAR(50),
+    Email VARCHAR(100),
+    PRIMARY KEY (Email)
+};
