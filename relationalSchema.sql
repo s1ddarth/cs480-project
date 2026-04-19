3622b09aeadb72fa64f303416de145692dadfdc8
Xhale2 <xhale2@uic.edu>
2026-04-19 18:50:20 -0500

    Update relationalSchema.sql

--- relationalSchema.sql
+++ relationalSchema.sql
@@ -1,12 +1,14 @@
+--3.8 Credit Card
 CREATE TABLE CreditCard {  --strong entity
     CreditCardNumber VARCHAR(30),
     ClientEmail VARCHAR(100),
     BillingAddressID INT,
     PRIMARY KEY (CreditCardNumber),
     FOREIGN KEY (ClientEmail) REFERENCES Client(Email),
-    --FOREIGN KEY (BillingAddressID) REFERENCES Address(zipcode)?
+    FOREIGN KEY (BillingAddressID) REFERENCES Address(Number)
 };
 
+--3.7 Address
 CREATE TABLE Address {   --strong entity
     Street VARCHAR(50),
     City VARCHAR(50),
@@ -14,13 +16,13 @@ CREATE TABLE Address {   --strong entity
     CreditCardNumber INT,
     ClientEmail VARCHAR(100),
     Hotel VARCHAR(100),
-    --Zipcode INT,
-    --PRIMARY KEY (Zipcode),
+    PRIMARY KEY (Number),
     FOREIGN KEY (ClientEmail) REFERENCES Client(Email),
     FOREIGN KEY (Hotel) REFERENCES Hotel(HoetelID),
     FOREIGN KEY (CreditCardNumber) REFERENCES CreditCard(CreditCardNumber)
 };
 
+--3.2 Client
 CREATE TABLE Client {   --strong entity
     Name VARCHAR(50),
     Email VARCHAR(100),
