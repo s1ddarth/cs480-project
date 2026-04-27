3f9f68fddf8258ff970ad0510689adfe9304b9cb
Siddarth Menon <33069694+s1ddarth@users.noreply.github.com>
2026-04-27 09:28:02 -0500

    DB Schema - Project Part 2 (#1)
    * syntax updates
    
    * formatting and comments
    
    * consolidated 3.1 and 3.4 into the main file
    
    * added NOTNULL checks for 3.1 and 3.2
    
    * Updated schema for 3.4 - hotel
    
    * updates to schema for 3.5
    
    * updates to credit card schema
    
    * rearranging table creations (im stupid)
    
    * removed foreign key to creditcard in address
    
    i think the prior implementation would cause a circular dependency
    
    also updated column type to match Hotel.HotelID
    
    * added reference to HotelID for foreign key in Review
    
    * added a README (project prompt)
    
    * review range is 0 to 10
    
    * updated HootelID in Booking and Review to match Hotel.HotelID
    
    * updated foreign key to reference ClientEmail instead of ClientID in Booking and Review

--- relationalSchema.sql
+++ relationalSchema.sql
@@ -1,68 +1,84 @@
---3.8 Credit Card
-CREATE TABLE CreditCard {  --strong entity
-    CreditCardNumber VARCHAR(30),
-    ClientEmail VARCHAR(100),
-    BillingAddressID INT,
-    PRIMARY KEY (CreditCardNumber),
-    FOREIGN KEY (ClientEmail) REFERENCES Client(Email),
-    FOREIGN KEY (BillingAddressID) REFERENCES Address(Number)
-};
+-- 3.1 Managers
+CREATE TABLE Managers (
+	Name VARCHAR(100) NOT NULL,
+	SSN CHAR(11) NOT NULL,
+	Email VARCHAR(100) NOT NULL,
+	PRIMARY KEY (SSN)
+);
 
---3.7 Address
-CREATE TABLE Address {   --strong entity
+-- 3.2 Client
+CREATE TABLE Client (
+	Name VARCHAR(100) NOT NULL,
+	Email VARCHAR(100) NOT NULL,
+	PRIMARY KEY (Email)
+);
+
+-- 3.4 Hotel
+CREATE TABLE Hotel (
+	Name VARCHAR(100) NOT NULL,
+	HotelID VARCHAR(15) NOT NULL,
+	Address VARCHAR(500) NOT NULL,
+	PRIMARY KEY (HotelID)
+);
+
+-- 3.5 Room
+CREATE TABLE Room (
+	RoomNumber INT NOT NULL,
+	HotelID VARCHAR(15) NOT NULL,
+	AccessMode VARCHAR(10) NOT NULL,
+	NumWindows INT NOT NULL DEFAULT 0,
+	LastRenovatedYear INT,
+	PRIMARY KEY (RoomNumber, HotelID),
+	FOREIGN KEY (HotelID) REFERENCES Hotel (HotelID),
+	CONSTRAINT chk_access CHECK (AccessMode IN ('lift', 'stairs'))
+);
+
+-- 3.7 Address
+CREATE TABLE Address (   --strong entity
     Street VARCHAR(50),
     City VARCHAR(50),
     Number INT,
     CreditCardNumber INT,
     ClientEmail VARCHAR(100),
-    Hotel VARCHAR(100),
+    Hotel VARCHAR(15),
     PRIMARY KEY (Number),
     FOREIGN KEY (ClientEmail) REFERENCES Client(Email),
-    FOREIGN KEY (Hotel) REFERENCES Hotel(HoetelID),
-    FOREIGN KEY (CreditCardNumber) REFERENCES CreditCard(CreditCardNumber)
-};
+    FOREIGN KEY (Hotel) REFERENCES Hotel(HotelID)
+);
 
---3.2 Client
-CREATE TABLE Client {   --strong entity
-    Name VARCHAR(50),
-    Email VARCHAR(100),
-    PRIMARY KEY (Email)
-};
+-- 3.8 Credit Card
+CREATE TABLE CreditCard (  --strong entity
+    CreditCardNumber VARCHAR(30) NOT NULL,
+    ClientEmail VARCHAR(100) NOT NULL,
+    BillingAddressID INT NOT NULL,
+    PRIMARY KEY (CreditCardNumber),
+    FOREIGN KEY (ClientEmail) REFERENCES Client(Email),
+    FOREIGN KEY (BillingAddressID) REFERENCES Address(Number)
+);
 
+-- 3.3 Booking
 CREATE TABLE Booking (
     BookingID INT,
     ClientEmail VARCHAR(100),
+    HotelID VARCHAR(15),
     RoomNumber INT,
     Price INT CHECK (Price >= 0),
     Dates DATETIME NOT NULL,
-    FOREIGN KEY (ClientID) REFERENCES Client(Email),
+    FOREIGN KEY (ClientEmail) REFERENCES Client(Email),
     FOREIGN KEY (RoomNumber) REFERENCES Room(RoomNumber),
     PRIMARY KEY (BookingID, ClientEmail, RoomNumber)
 );
 
-CREATE TABLE Room (
-    RoomNumber INT,
-    HotelID INT,
-    AccessMode VARCHAR(10),
-    NumWindows INT DEFAULT 0,
-    LastRenovatedYear INT,
-    PRIMARY KEY  (RoomNumber, HotelID),
-    FOREIGN KEY (HotelID) REFERENCES Hotel,
-    CONSTRAINT chk_access CHECK (AccessMode IN ('lift', 'stairs')),
-);
-
+-- 3.6 Review
 CREATE TABLE Review (
     ReviewID INT,
     Message VARCHAR(500),
     Rating INT,
     ClientEmail VARCHAR(100),
-    HotelID INT,
+    HotelID VARCHAR(15),
     PRIMARY KEY  (ReviewID, HotelID),
-    FOREIGN KEY (HotelID) REFERENCES Hotel,
-    FOREIGN KEY (ClientID) REFERENCES Client(Email),
-    CONSTRAINT chk_rating CHECK (Rating BETWEEN 1 AND 5)
+    FOREIGN KEY (HotelID) REFERENCES Hotel(HotelID),
+    FOREIGN KEY (ClientEmail) REFERENCES Client(Email),
+    CONSTRAINT chk_rating CHECK (Rating BETWEEN 0 AND 10)
 );
-
-
-
 
